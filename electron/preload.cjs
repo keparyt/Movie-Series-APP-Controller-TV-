7696bf2b65d394f650c5105e8496e087d7e439c0
An Unknown Person <63467669+keparyt@users.noreply.github.com>
2026-08-16 03:19:05 -0400

    Expose secure controller input bridge

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -4,5 +4,6 @@ contextBridge.exposeInMainWorld('electronAPI', Object.freeze({
   isElectron: true,
   platform: process.platform,
   enterFullscreen: () => ipcRenderer.invoke('enter-fullscreen'),
-  exitFullscreen: () => ipcRenderer.invoke('exit-fullscreen')
+  exitFullscreen: () => ipcRenderer.invoke('exit-fullscreen'),
+  sendControllerKey: (key) => ipcRenderer.invoke('controller-key', key)
 }));
